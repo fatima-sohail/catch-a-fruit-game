24c5a9b06f5b077e670974eb864cf32bc46f471b
unknown <fatimasohailhunzai@gmail.com>
2023-06-11 10:55:00 -0400

    added code to move the basket horizontally within screen bounds

--- logic.js
+++ logic.js
@@ -1,7 +1,35 @@
 //we want to move basket horizontally using keyboard keys
-let basket = document.getElementById('basket')
 
-function left(){
-    basket.style.left = '40%'
+let currentPosition = window.innerWidth / 2
+let horizontalMovement = function horizontalMovement(event) {
+    let basket = document.getElementById('basket')
+    // currentPosition = parseInt(basket.style.left) ;
+    if (event.key === 'ArrowLeft') {
+        // Move the basket to the left
+        currentPosition -= 10;
+    } else if (event.key === 'ArrowRight') {
+        // Move the basket to the right
+        currentPosition += 10;
+    }
+    //Basket goes out of screen width, continues to go left and right
+    //to keep the basket within screen view
+    var screenWidth = window.innerWidth
+    var divWidth = basket.offsetWidth
+
+    if(currentPosition < 0){
+        currentPosition = 0;
+    }else if(currentPosition + divWidth > screenWidth){
+        currentPosition = screenWidth-divWidth
+    }
+
+    //update position of the basket
+    basket.style.left = currentPosition + 'px'
 }
-basket.addEventListener('keydown', left)
+//create eventListener on element
+//eventListener needs an event to trigger function when left key is pressed on the keyboard
+//that event is called 'onkeydown' 
+document.addEventListener('keydown', horizontalMovement)
+
+
+
+
